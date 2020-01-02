6b0ffac5a12c0abe78dc0098b8be24d5d7cb0e36
Argho Das <aiub2016@yahoo.com>
2020-01-03 00:26:01 +0600

    C# Scripts

---
+++ ModelTouch.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class ModelTouch : MonoBehaviour {
+
+    public GameObject videoPlayerPlane;
+    private VideoPlayer videoPlayer;
+    private Animator animator;
+    private Animator videoAnimator;
+    // Use this for initialization
+    void Start () {
+        animator = GetComponent<Animator>();
+        videoAnimator = videoPlayerPlane.GetComponent<Animator>(); 
+        videoPlayer = videoPlayerPlane.GetComponent<VideoPlayer>();
+    }
+	
+
+
+    void OnMouseDown()
+    {
+        animator.SetBool("Scale",true);
+        Invoke("PlayVideo",.5f);       
+    }
+
+    void PlayVideo()
+    {
+        videoPlayer.Play();
+        videoAnimator.SetBool("Scale", true);
+    }
+}
